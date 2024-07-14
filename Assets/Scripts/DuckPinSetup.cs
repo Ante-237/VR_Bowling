21efa4de43dfc69e99c7f0f65d18c700228a9ce6
anTe <a.nwalahnjie@alustudent.com>
2024-07-14 20:36:04 +0200

    duckPinsetup

--- Assets/Scripts/DuckPinSetup.cs
+++ Assets/Scripts/DuckPinSetup.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class DuckPinSetup : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject duckPinPrefab;
+    [SerializeField] private Transform duckPinSpawnStartPoint;
+    [SerializeField] private Transform duckPinSpawnParent;
+    [SerializeField][Range(0, 50f)] private float distanceBtwDuckPin = 30.5f;
+
+
+    private const int numberOfDuckPin = 10;
+    private GameObject duckPinInstance; 
+
+    public void Start()
     {
-        
+        for(int i = 0; i < numberOfDuckPin; i++)
+        {
+            duckPinInstance = Instantiate(duckPinPrefab);
+        }
+
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
